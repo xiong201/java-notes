361778c1e8b47f3cdad19463620a99c2e0d7dbe4
zhangzhenxiong <1963942081@qq.com>
2023-10-26 14:15:28 +0800

    【软技能】方法论（思考框架、GTD）

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,8 @@ module.exports = {
     nav: [
       { text: '首页', link: '/' },
       {text:'Java 知识体系', link:'/pages/0e35d1/'},
-      {text:'Java 面试宝典', link:'/pages/5547aa/'}
+      {text:'Java 面试宝典', link:'/pages/5547aa/'},
+      {text:'软技能', link:'/pages/1eb629/'}
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     // logo: '/img/logo.png', // 导航栏logo
